74e753f489231641a2918ccd1d11fe827af704db
Andres ResetDigital <andres@resetdigital.co>
2023-03-07 11:02:46 -0300

    creacion clase auto

---
+++ auto.ts
@@ -0,0 +1,13 @@
+export class Auto {
+    nombre: string;
+    marca: string;
+    anio: number;
+    modelo: string;
+    
+    constructor(nombre:string, marca:string, anio:number, modelo:string){
+        this.nombre = nombre;
+        this.marca = marca;
+        this.anio = anio;
+        this.modelo = modelo;
+    }
+    }
